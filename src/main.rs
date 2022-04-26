9a6aee220ff9c6ca1129d320a6d6549dbe6a1d33
MoriGM <30366747+MoriGM@users.noreply.github.com>
2022-04-26 11:30:03 +0200

    Add file repo.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
-use git2::Repository;
+pub mod repo;
 
 fn main() {
-    let repo = match Repository::init("/home/max/Code/rust/book/hello_world/") {
+    let repo = match repo::GitRepo::new("/home/max/Code/rust/book/hello_world/") {
         Ok(repo) => repo,
-        Err(e) => panic!("failed to init: {}", e),
+        Err(err) => panic!("{}", err)
     };
     
-    let mut revs = match repo.revwalk() {
-        Ok(rev) => rev,
-        Err(e) => panic!("failed to init: {}", e)
-    };
-    
-    revs.push_head().unwrap();
-    
-    revs.for_each(|rev| {
-        print!("{:?}", rev.ok());
+    repo.logs().for_each(|rev| {
+        println!("{}", rev);
     });
 }
---
+++ src/repo.rs
@@ -0,0 +1,36 @@
+use git2::Repository;
+
+pub struct GitRepo {
+    repo: Repository
+}
+
+impl GitRepo {
+    pub fn new(path: &str) -> Result<GitRepo, String> {
+        let repo = Repository::open(path);
+        
+        if repo.is_err() {
+           return Err("Canno't find repo in path".to_string());
+        }
+        
+        let repo = repo.unwrap();
+        
+        Ok(GitRepo{repo})
+    }
+    
+    pub fn logs(&self) -> std::vec::IntoIter<String> {
+        let mut revs = match self.repo.revwalk() {
+            Ok(rev) => rev,
+            Err(e) => panic!("failed to init: {}", e)
+        };
+        
+        revs.push_head().unwrap();
+        
+        let mut log_strings = vec![];
+        
+        revs.for_each(|rev| {
+            log_strings.push(format!("{}", rev.unwrap()))
+        });
+        
+        log_strings.into_iter()
+    } 
+}
