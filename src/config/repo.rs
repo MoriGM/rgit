f85fb9a4c56d7fcd0884e0130ebbaab20d304012
MoriGM <30366747+MoriGM@users.noreply.github.com>
2022-04-28 13:20:40 +0200

    Add rgit_repos.toml to register all the repos

---
+++ src/config/repo.rs
@@ -0,0 +1,22 @@
+use serde::Deserialize;
+
+use std::fs;
+
+#[derive(Debug, Deserialize)]
+pub struct CofigRepos {
+    pub repos: Option<Vec<CofigRepo>>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CofigRepo {
+    pub name: Option<String>,
+    pub path: Option<String>
+}
+
+pub fn get_repos() -> CofigRepos {
+    let content = match fs::read_to_string("rgit_repos.toml") {
+        Ok(content) => content,
+        Err(_) => panic!("File is missing!")
+    };
+    toml::from_str(content.as_str()).expect("Content is incorrect!")
+}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod config;
 #[macro_use] extern crate rocket;
 
 use rocket::http::{Status, ContentType};
-use rocket::Config;
 use clap::Parser;
 
 use crate::repo::GitRepo;
@@ -23,7 +22,22 @@ fn index() -> &'static str {
 
 #[get("/<repo>")]
 fn web_repo(repo: &str) -> (Status, (ContentType, String)) {
-    let repo = GitRepo::new("/home/max/Code/rust/book/hello_world/");
+    let repos = match config::repo::get_repos().repos {
+        Some(repos) => repos,
+        None => panic!("Missing no repo is registered in the rgit_repos.toml")
+    };
+    
+    let repo = String::from(repo);
+    
+    let repo_path = repos.iter().find(|config_repo| config_repo.name.as_ref().expect("[[repos]] in rgit_repos.toml is missing name") == &repo);
+    
+    if repo_path.is_none() {
+        return (Status::NotFound, (ContentType::HTML, String::new()));
+    } 
+    
+    let repo_path = repo_path.unwrap().path.as_ref().expect("[[repos]] in rgit_repos.toml is missing its path");
+    
+    let repo = GitRepo::new(repo_path.as_str());
     
     let mut body = String::from("");
     
@@ -39,9 +53,9 @@ fn web_repo(repo: &str) -> (Status, (ContentType, String)) {
 fn rocket() -> _ {
     let cli = Cli::parse();
     
-    let mut config = Config::default();
+    let mut config = rocket::Config::default();
     config.port = cli.port.parse::<u16>().unwrap();
-    
+
     rocket::build()
         .mount("/", routes![index])
         .mount("/repo", routes![web_repo])
