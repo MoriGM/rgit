d4f2123d81d2a61f50337d4a8a5e123c178a010b
MoriGM <30366747+MoriGM@users.noreply.github.com>
2022-06-03 23:15:42 +0200

    Fix error in which css is send as content type htnk

--- src/pages/statics/mod.rs
+++ src/pages/statics/mod.rs
@@ -6,7 +6,7 @@ use tera::Tera;
 #[get("/<path>/<file>")]
 pub fn index<'a>(_tera: &State<Tera>, path: &str, file: &str) -> (Status, (ContentType, &'a str)) {
     if path == "css" && file == "main.css" {
-        return (Status::Ok, (ContentType::HTML, include_str!("../../../statics/css/main.css")));
+        return (Status::Ok, (ContentType::CSS, include_str!("../../../statics/css/main.css")));
     }
     
     (Status::NotFound, (ContentType::HTML, ""))
