610ce6c243a530410e5f9b38d9304e7a620882bb
MoriGM <30366747+MoriGM@users.noreply.github.com>
2022-06-03 23:40:26 +0200

    Add test test_repo_refresh_has_commits

--- src/tests/repo_tests.rs
+++ src/tests/repo_tests.rs
@@ -29,3 +29,19 @@ fn test_repo_refresh() {
     
     assert_eq!(repo.is_ok(), true);
 }
+
+#[test]
+fn test_repo_refresh_has_commits() {
+    let repo = GitRepo::new("./");
+    
+    assert_eq!(repo.is_ok(), true);
+    
+    let repo = repo.unwrap().refresh();
+    
+    assert_eq!(repo.is_ok(), true);
+    
+    let repo = repo.unwrap();
+    
+    let commits = repo.logs();
+    assert_eq!(commits.len() > 0, true);
+}
